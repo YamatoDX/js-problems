a7e00136319c08503d4d01cf58a7590c5426d8a2
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-19 20:15:34 +0600

    intersectionArray file added

---
+++ intersectionArray.js
@@ -0,0 +1,23 @@
+// https://www.youtube.com/watch?v=Wo7dbhMEw0o&list=PLko32sysgiEPJL_t3A7UAZQIUUixV_sw2&index=5
+// https://leetcode.com/problems/intersection-of-two-arrays/
+
+var intersection1 = function (nums1, nums2) {
+  return Array.from(new Set(nums1.filter((each) => nums2.includes(each))));
+};
+
+/**
+ *
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @returns {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  let finalResult = [];
+  let nums1_uniques = new Set(nums1);
+  for (let each of nums2) {
+    if (!finalResult.includes(each) && nums1_uniques.has(each)) {
+      finalResult.push(each);
+    }
+  }
+  return finalResult;
+};
--- isSubsequence.js
+++ isSubsequence.js
@@ -1,27 +1,26 @@
 // https://leetcode.com/problems/is-subsequence/submissions/825527987/
 // https://www.youtube.com/watch?v=RqrGVXvyxZ0&t=356s
 
-
 /**
- * 
+ *
  * @param {string} s - short string to be checked
  * @param {string} t - long string whose subsequence we are figuring out
  * @return {boolean} - true if subsequence else false
  */
- var isSubsequence = function(s, t) {
-    // s is short string
-    // t is the long string
-    let [pointerOne,pointerTwo] = [0,0]
-    while(!!s[pointerOne] && !!t[pointerTwo]){
-        if(s[pointerOne] === t[pointerTwo]){
-            pointerOne++
-            pointerTwo++
-            continue;
-        }
-        if(s[pointerOne] !== t[pointerTwo]){
-            pointerTwo++
-            continue;
-        }
+var isSubsequence = function (s, t) {
+  // s is short string
+  // t is the long string
+  let [pointerOne, pointerTwo] = [0, 0];
+  while (!!s[pointerOne] && !!t[pointerTwo]) {
+    if (s[pointerOne] === t[pointerTwo]) {
+      pointerOne++;
+      pointerTwo++;
+      continue;
     }
-    return !s[pointerOne] ? true : false
+    if (s[pointerOne] !== t[pointerTwo]) {
+      pointerTwo++;
+      continue;
+    }
+  }
+  return !s[pointerOne] ? true : false;
 };
