d19365e30331604cbcc51db88e9977b006e1bf51
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-20 03:18:50 +0600

    missingNumber file added

--- intersectionArray.js
+++ intersectionArray.js
@@ -9,7 +9,7 @@ var intersection1 = function (nums1, nums2) {
  *
  * @param {number[]} nums1
  * @param {number[]} nums2
- * @returns {number[]}
+ * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
   let finalResult = [];
---
+++ missingNumber.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=4LrVhAxJUsA&list=PLko32sysgiEPJL_t3A7UAZQIUUixV_sw2&index=6
+// https://leetcode.com/problems/missing-number/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const currentSum = nums.reduce((prev, current) => prev + current, 0);
+  let totalSum = nums.length;
+  for (let index = 0; index < nums.length; index++) {
+    totalSum += index;
+  }
+  return totalSum - currentSum;
+};
