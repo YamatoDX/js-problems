d65c7047b96278a3da73fb4231139a88119d2ba0
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-21 04:08:19 +0600

    var changed to const

--- trailingZeroes.js
+++ trailingZeroes.js
@@ -5,7 +5,7 @@
  * @param {number} n
  * @return {number}
  */
-var trailingZeroes = function (n) {
+const trailingZeroes = function (n) {
   let total = 0;
   while (n >= 5) {
     let current = Math.floor(n / 5);
