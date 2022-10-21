1d1383812cf55e865af985a11ebcba39e6801850
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-22 05:12:42 +0600

    LongestSubstring medium problem added

---
+++ medium/3LongestSubstring.js
@@ -0,0 +1,29 @@
+// https://www.youtube.com/watch?v=WKTgajDkVcA&list=PLko32sysgiEO7o5jjl-cTIXAvneIY3_U8&index=3&ab_channel=TerribleWhiteboard
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let set = new Set();
+  let left = 0;
+  let right = 0;
+  var max = 0;
+
+  while (right < s.length) {
+    if (!set.has(s[right])) {
+      set.add(s[right]);
+      console.log("max before", max);
+      max = max > set.size ? max : set.size;
+      right++;
+    } else {
+      set.delete(s[left]);
+      left++;
+    }
+  }
+  return max;
+};
+
+// const result = lengthOfLongestSubstring("aabbcccddabc");
+// console.log("result is", result);
