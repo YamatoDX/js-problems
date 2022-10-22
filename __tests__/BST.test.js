6d547a3fbe6b12353e5c2a24dbddb71c2a37818f
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-23 01:41:18 +0600

    BST test passed and updated

--- __tests__/BST.test.js
+++ __tests__/BST.test.js
@@ -10,4 +10,22 @@ test("Testing BST find, insert", () => {
   expect(myBST.BFS()).toStrictEqual([
     99, 11, 100, 4, 22, 200, 6, 33, 55, 40, 75,
   ]);
+  expect(myBST.DFSPreOrder()).toStrictEqual([
+    99, 11, 4, 6, 22, 33, 55, 40, 75, 100, 200,
+  ]);
+  expect(myBST.DFSPreOrderIterative()).toStrictEqual([
+    99, 11, 4, 6, 22, 33, 55, 40, 75, 100, 200,
+  ]);
+  expect(myBST.DFSInOrder()).toStrictEqual([
+    4, 6, 11, 22, 33, 40, 55, 75, 99, 100, 200,
+  ]);
+  expect(myBST.DFSInOrderIterative()).toStrictEqual([
+    4, 6, 11, 22, 33, 40, 55, 75, 99, 100, 200,
+  ]);
+  expect(myBST.DFSPostOrder()).toStrictEqual([
+    6, 4, 40, 75, 55, 33, 22, 11, 200, 100, 99,
+  ]);
+  expect(myBST.DFSPostOrderIterative()).toStrictEqual([
+    6, 4, 40, 75, 55, 33, 22, 11, 200, 100, 99,
+  ]);
 });
