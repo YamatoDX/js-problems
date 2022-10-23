05e608e80bc3603ef7f6f8e5ac35532753344a3d
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-24 02:26:28 +0600

    prettier config files added

--- BST/index.js
+++ BST/index.js
@@ -1,252 +1,252 @@
 /* Queue Starts*/
 class Queue {
-  constructor() {
-    this.elements = [];
-    this.length = 0;
-  }
-  enqueue(val) {
-    this.elements.unshift(val);
-    this.length += 1;
-    return true;
-  }
-  dequeue() {
-    const dElement = this.elements.pop();
-    this.length -= 1;
-    return dElement;
-  }
+    constructor() {
+        this.elements = [];
+        this.length = 0;
+    }
+    enqueue(val) {
+        this.elements.unshift(val);
+        this.length += 1;
+        return true;
+    }
+    dequeue() {
+        const dElement = this.elements.pop();
+        this.length -= 1;
+        return dElement;
+    }
 }
 /* Queue Ends */
 
 class BSTNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BST {
-  constructor() {
-    this.root = null;
-  }
-
-  insert(val) {
-    const newNode = new BSTNode(val);
-    if (!this.root) {
-      this.root = newNode;
-      return true;
+    constructor() {
+        this.root = null;
     }
-    let current = this.root;
-    while (true) {
-      if (current === val) {
-        return false;
-      }
-      if (val < current.val) {
-        if (!current.left) {
-          current.left = newNode;
-          return true;
+
+    insert(val) {
+        const newNode = new BSTNode(val);
+        if (!this.root) {
+            this.root = newNode;
+            return true;
         }
-        current = current.left;
-        continue;
-      }
-      if (val > current.val) {
-        if (!current.right) {
-          current.right = newNode;
-          return true;
+        let current = this.root;
+        while (true) {
+            if (current === val) {
+                return false;
+            }
+            if (val < current.val) {
+                if (!current.left) {
+                    current.left = newNode;
+                    return true;
+                }
+                current = current.left;
+                continue;
+            }
+            if (val > current.val) {
+                if (!current.right) {
+                    current.right = newNode;
+                    return true;
+                }
+                current = current.right;
+                continue;
+            }
         }
-        current = current.right;
-        continue;
-      }
     }
-  }
 
-  find(val) {
-    if (!this.root) {
-      return undefined;
+    find(val) {
+        if (!this.root) {
+            return undefined;
+        }
+        let current = this.root;
+        let found = false;
+        while (current && !found) {
+            if (val === current.val) {
+                found = true;
+                break;
+            }
+            if (val < current.val) {
+                current = current.left;
+                continue;
+            }
+            if (val > current.val) {
+                current = current.right;
+                continue;
+            }
+        }
+        return found;
     }
-    let current = this.root;
-    let found = false;
-    while (current && !found) {
-      if (val === current.val) {
-        found = true;
-        break;
-      }
-      if (val < current.val) {
-        current = current.left;
-        continue;
-      }
-      if (val > current.val) {
-        current = current.right;
-        continue;
-      }
-    }
-    return found;
-  }
 
-  BFS() {
-    if (!this.root) {
-      return [];
+    BFS() {
+        if (!this.root) {
+            return [];
+        }
+        let q = new Queue();
+        q.enqueue(this.root);
+        let visited = [];
+        while (q.length > 0) {
+            let dNode = q.dequeue();
+            if (dNode.left) {
+                q.enqueue(dNode.left);
+            }
+            if (dNode.right) {
+                q.enqueue(dNode.right);
+            }
+            visited.push(dNode.val);
+        }
+        return visited;
     }
-    let q = new Queue();
-    q.enqueue(this.root);
-    let visited = [];
-    while (q.length > 0) {
-      let dNode = q.dequeue();
-      if (dNode.left) {
-        q.enqueue(dNode.left);
-      }
-      if (dNode.right) {
-        q.enqueue(dNode.right);
-      }
-      visited.push(dNode.val);
-    }
-    return visited;
-  }
 
-  DFSPreOrder() {
-    // this can be used to reconstruct the BST
-    if (!this.root) {
-      return [];
-    }
-    let visited = [];
+    DFSPreOrder() {
+        // this can be used to reconstruct the BST
+        if (!this.root) {
+            return [];
+        }
+        let visited = [];
 
-    function traverse(currentNode) {
-      if (!currentNode) {
-        return;
-      }
-      visited.push(currentNode.val);
-      if (currentNode.left) {
-        traverse(currentNode.left);
-      }
-      if (currentNode.right) {
-        traverse(currentNode.right);
-      }
+        function traverse(currentNode) {
+            if (!currentNode) {
+                return;
+            }
+            visited.push(currentNode.val);
+            if (currentNode.left) {
+                traverse(currentNode.left);
+            }
+            if (currentNode.right) {
+                traverse(currentNode.right);
+            }
+        }
+        traverse(this.root);
+        return visited;
     }
-    traverse(this.root);
-    return visited;
-  }
 
-  DFSInOrder() {
-    // returns array in increasing order
-    if (!this.root) {
-      return [];
+    DFSInOrder() {
+        // returns array in increasing order
+        if (!this.root) {
+            return [];
+        }
+        let visited = [];
+        function traverse(currentNode) {
+            if (!currentNode) {
+                return;
+            }
+            if (currentNode.left) {
+                traverse(currentNode.left);
+            }
+            visited.push(currentNode.val);
+            if (currentNode.right) {
+                traverse(currentNode.right);
+            }
+        }
+        traverse(this.root);
+        return visited;
     }
-    let visited = [];
-    function traverse(currentNode) {
-      if (!currentNode) {
-        return;
-      }
-      if (currentNode.left) {
-        traverse(currentNode.left);
-      }
-      visited.push(currentNode.val);
-      if (currentNode.right) {
-        traverse(currentNode.right);
-      }
-    }
-    traverse(this.root);
-    return visited;
-  }
 
-  DFSPostOrder() {
-    if (!this.root) {
-      return [];
+    DFSPostOrder() {
+        if (!this.root) {
+            return [];
+        }
+        let visited = [];
+        function traverse(currentNode) {
+            if (!currentNode) {
+                return;
+            }
+            if (currentNode.left) {
+                traverse(currentNode.left);
+            }
+            if (currentNode.right) {
+                traverse(currentNode.right);
+            }
+            visited.push(currentNode.val);
+        }
+        traverse(this.root);
+        return visited;
     }
-    let visited = [];
-    function traverse(currentNode) {
-      if (!currentNode) {
-        return;
-      }
-      if (currentNode.left) {
-        traverse(currentNode.left);
-      }
-      if (currentNode.right) {
-        traverse(currentNode.right);
-      }
-      visited.push(currentNode.val);
-    }
-    traverse(this.root);
-    return visited;
-  }
 
-  // DFS Iterative Methods
-  DFSPreOrderIterative() {
-    if (!this.root) {
-      return [];
+    // DFS Iterative Methods
+    DFSPreOrderIterative() {
+        if (!this.root) {
+            return [];
+        }
+        let temp = [this.root];
+        let visited = [];
+        while (temp.length > 0) {
+            const current = temp.pop();
+            if (current.right) temp.push(current.right);
+            if (current.left) temp.push(current.left);
+            visited.push(current.val);
+        }
+        return visited;
     }
-    let temp = [this.root];
-    let visited = [];
-    while (temp.length > 0) {
-      const current = temp.pop();
-      if (current.right) temp.push(current.right);
-      if (current.left) temp.push(current.left);
-      visited.push(current.val);
-    }
-    return visited;
-  }
 
-  DFSInOrderIterative() {
-    if (!this.root) {
-      return [];
+    DFSInOrderIterative() {
+        if (!this.root) {
+            return [];
+        }
+        let s = [];
+        let finalResult = [];
+        let current = this.root;
+        while (s.length !== 0 || current) {
+            if (current) {
+                s.push(current);
+                current = current.left;
+            } else {
+                current = s.pop();
+                finalResult.push(current.val);
+                current = current.right;
+            }
+        }
+        return finalResult;
     }
-    let s = [];
-    let finalResult = [];
-    let current = this.root;
-    while (s.length !== 0 || current) {
-      if (current) {
-        s.push(current);
-        current = current.left;
-      } else {
-        current = s.pop();
-        finalResult.push(current.val);
-        current = current.right;
-      }
-    }
-    return finalResult;
-  }
 
-  DFSPostOrderIterative() {
-    if (!this.root) {
-      return [];
-    }
-    let s = [this.root];
-    let finalResult = [];
+    DFSPostOrderIterative() {
+        if (!this.root) {
+            return [];
+        }
+        let s = [this.root];
+        let finalResult = [];
 
-    while (s.length !== 0) {
-      let current = s.pop();
-      finalResult.push(current.val);
-      if (current.left) s.push(current.left);
-      if (current.right) s.push(current.right);
+        while (s.length !== 0) {
+            let current = s.pop();
+            finalResult.push(current.val);
+            if (current.left) s.push(current.left);
+            if (current.right) s.push(current.right);
+        }
+        return finalResult.reverse();
     }
-    return finalResult.reverse();
-  }
 
-  LevelOrderBottom() {
-    if (!this.root) {
-      return [];
+    LevelOrderBottom() {
+        if (!this.root) {
+            return [];
+        }
+        let result = [];
+        let q = new Queue();
+        q.enqueue(this.root);
+        while (q.length !== 0) {
+            let nodeCount = q.length;
+            let rowResult = [];
+            while (nodeCount > 0) {
+                let current = q.dequeue();
+                if (current.left) q.enqueue(current.left);
+                if (current.right) q.enqueue(current.right);
+                rowResult.push(current.val);
+                nodeCount--;
+            }
+            result.unshift(rowResult);
+        }
+        return result;
     }
-    let result = [];
-    let q = new Queue();
-    q.enqueue(this.root);
-    while (q.length !== 0) {
-      let nodeCount = q.length;
-      let rowResult = [];
-      while (nodeCount > 0) {
-        let current = q.dequeue();
-        if (current.left) q.enqueue(current.left);
-        if (current.right) q.enqueue(current.right);
-        rowResult.push(current.val);
-        nodeCount--;
-      }
-      result.unshift(rowResult);
-    }
-    return result;
-  }
 }
 
 const myBST = new BST();
 const allInputs = [99, 11, 22, 33, 4, 55, 6, 100, 40, 75, 200];
-allInputs.forEach((eachElement) => myBST.insert(eachElement));
+allInputs.forEach(eachElement => myBST.insert(eachElement));
 console.log("input is", allInputs);
 console.log("BFS is", myBST.BFS());
 console.log("DFSPreOrder is", myBST.DFSPreOrder());
@@ -258,7 +258,7 @@ console.log("DFSInOrderIterative is", myBST.DFSInOrderIterative());
 console.log("LevelOrderBottom", myBST.LevelOrderBottom());
 
 module.exports = {
-  BST,
-  BSTNode,
-  Queue,
+    BST,
+    BSTNode,
+    Queue
 };
