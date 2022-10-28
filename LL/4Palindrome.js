ea8df601a809d132e509ba77e1f8b47e3a69a93d
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-10-28 07:45:02 +0600

    reverseLL added

---
+++ LL/4Palindrome.js
@@ -0,0 +1,15 @@
+// https://www.youtube.com/watch?v=vHam6riSavo&list=PLko32sysgiEP903b1Zvo9_qXJ1WvkW7-9&index=4&ab_channel=TerribleWhiteboard
+// https://leetcode.com/problems/palindrome-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {};
---
+++ LL/8reverseLL.js
@@ -0,0 +1,27 @@
+// https://www.youtube.com/watch?v=jXArj8RBq0Q&list=PLko32sysgiEP903b1Zvo9_qXJ1WvkW7-9&index=9&ab_channel=TerribleWhiteboard
+// https://leetcode.com/problems/reverse-linked-list/submissions/831756898/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    if (!head) return null;
+
+    let prevNode = null;
+
+    while (head !== null) {
+        let nextNode = head.next;
+        head.next = prevNode;
+        prevNode = head;
+        head = nextNode;
+    }
+    return prevNode;
+};
