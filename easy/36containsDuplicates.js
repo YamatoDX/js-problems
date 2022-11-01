be12dfd94b2cf40e168d13f7a55ac933bb75444c
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-11-02 01:39:49 +0600

    containsDuplicateII solved

--- easy/36containsDuplicates.js
+++ easy/36containsDuplicates.js
@@ -6,4 +6,33 @@
  * @param {number} k
  * @return {boolean}
  */
-let containsNearbyDuplicate = function (nums, k) {};
+let containsNearbyDuplicate = function (nums, k) {
+    // usage of Map is more performant
+    let numsMap = new Map();
+    for (let index = 0; index < nums.length; index++) {
+        const current = nums[index];
+        if (numsMap.has(current) && index - numsMap.get(current) <= k) {
+            return true;
+        } else {
+            numsMap.set(current, index);
+        }
+    }
+    return false;
+
+    // let numsMap = {}
+    // for(let index = 0; index < nums.length; index++){
+    //     let current = nums[index];
+    //     if(numsMap[current] !== null){
+    //         const difference = index - numsMap[current];
+    //         if(difference <= k){
+    //             return true
+    //         }else{
+    //             numsMap[current] = index;
+    //             continue;
+    //         }
+    //     }else{
+    //         numsMap[current] = index;
+    //     }
+    // }
+    // return false;
+};
