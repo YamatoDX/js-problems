1bc0f9c0483cab30e7fc4ae3c2df48d94741d64c
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-11-01 19:50:02 +0600

    easy 36 incompleted

---
+++ easy/36containsDuplicates.js
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=Fmmzy5Jg-Mw&list=PLko32sysgiEPJL_t3A7UAZQIUUixV_sw2&index=36&ab_channel=TerribleWhiteboard
+// https://leetcode.com/problems/contains-duplicate-ii/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+let containsNearbyDuplicate = function (nums, k) {};
