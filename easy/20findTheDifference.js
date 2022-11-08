7da55d3184ca8a27b22ef88641820d62eb4ddd56
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-11-08 07:06:39 +0600

    20findTheDifference added

---
+++ easy/20findTheDifference.js
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=oFZLJ5Y5E48&list=PLko32sysgiEPJL_t3A7UAZQIUUixV_sw2&index=20&ab_channel=TerribleWhiteboard
+// https://leetcode.com/problems/find-the-difference/submissions/839059002/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function (s, t) {
+    let sMap = {};
+    for (let each of s) {
+        !sMap[each] ? (sMap[each] = 1) : (sMap[each] += 1);
+    }
+    for (let each of t) {
+        if (!sMap[each]) return each;
+        if (sMap[each]) {
+            sMap[each] -= 1;
+        }
+    }
+    return "";
+};
